39285fc8e5d67867e21f42ed49cb501abe54d55d
rameez471 <yashpathak29@gmail.com>
2020-09-05 15:03:28 +0530

    Loss Function

--- model/yolo.py
+++ model/yolo.py
@@ -162,3 +162,94 @@ class YoloLoss(object):
         self.valid_anchors_wh = valid_anchors_wh
         self.lambda_coord = 5.0
         self.lambda_noobj = 0.5
+
+    def __call__(self,y_true,y_pred):
+        #Split y_pred into xy,wh,objectness and num_classes
+
+        pred_xy_rel = tf.sigmoid(y_pred[...,0:2])
+        pred_wh_rel = y_pred[...,2:4]
+
+        pred_box_abs, pred_obj, pred_class = get_absolute_yolo_box(
+            y_pred,self.valid_anchors_wh,self.num_classes
+        )
+        pred_box_abs = xywh_to_x1x2y1y1(pred_box_abs)
+
+        true_xy_abs, true_wh_abs,true_obj,true_class = tf.split(
+            y_true,(2,2,1,self.num_classes),axis=-1)
+        true_box_abs = tf.concat([true_xy_abs,true_wh_abs],axis=-1)
+        true_box_abs = xywh_to_x1x2y1y1(true_box_abs)
+
+        true_box_rel = get_absolute_yolo_box(y_true,self.valid_anchors_wh)
+        true_xy_rel = true_box_rel[...,0:2]
+        true_wh_rel = true_box_abs[...,2:4]
+
+        weight = 2 - true_wh_abs[...,0] * true_wh_abs[...,1]
+
+        xy_loss = self.calc_xy_loss(true_obj,true_xy_rel,pred_xy_rel,weight)
+        wh_loss = self.calc_wh_loss(true_obj,true_wh_rel,pred_wh_rel,weight)
+        class_loss = self.calc_class_loss(true_obj,true_class,pred_class)
+
+        ignore_mask = self.calc_ignore_mask(true_obj,true_box_abs,pred_box_abs)
+        obj_loss = self.calc_obj_loss(true_obj,pred_obj,ignore_mask)
+
+        return xy_loss + wh_loss + class_loss + obj_loss,(xy_loss,wh_loss,
+                                                          class_loss,obj_loss)
+
+    def calc_ignore_mask(self,true_obj,true_box,pred_box):
+
+        true_box_shape = tf.shape(true_box)
+        pred_box_shape = tf.shape(pred_box)
+
+        true_box = tf.reshape(true_box,[true_box_shape[0],-1,4])
+        true_box = tf.sort(true_box,axis=1,direction='DESCENDING')
+
+        true_box = true_box[:,0:100,:]
+        pred_box = tf.reshape(pred_box,[pred_box_shape[0],-1,4])
+
+        iou = broadcast_iou(pred_box,true_box)
+        
+        best_iou = tf.reduce_max(iou,axis=-1)
+        best_iou = tf.reshape(best_iou,[pred_box_shape[0],pred_box_shape[1],pred_box_shape[2],pred_box_shape[3]])
+
+        ignore_mask = tf.cast(best_iou < self.ignore_threshold,tf.float32)
+        ignore_mask = tf.expand_dims(ignore_mask,axis=-1)
+
+        return ignore_mask
+
+    def clac_obj_loss(self,true_obj,pred_obj,ignore_mask):
+
+        obj_entropy = binary_crossentropy(pred_obj,true_obj)
+        noobj_loss = (1-true_obj)*obj_entropy * ignore_mask
+
+        obj_loss = tf.reduce_sum(noobj_loss,axis=(1,2,3,4))
+        noobj_loss = tf.reduce_sum(noobj_loss,axis=(1,2,3,4)) * self.lambda_noobj
+
+        return obj_loss + noobj_loss
+
+    def calc_class_loss(self,true_obj,true_class,pred_class):
+
+        class_loss = binary_crossentropy(pred_class,true_class)
+        class_loss = true_obj * class_loss
+        class_loss = tf.reduce_sum(class_loss,axis=(1,2,3,4))
+
+        return class_loss
+
+    def calc_xy_loss(self,true_obj,true_xy,pred_xy,weight):
+
+        xy_loss = tf.reduce_sum(tf.square(true_xy - pred_xy),axis=-1)
+        true_obj = tf.squeeze(true_obj,axis=-1)
+
+        xy_loss = true_obj * xy_loss * weight
+        xy_loss = tf.reduce_sum(xy_loss,axis=(1,2,3)) * self.lambda_coord
+
+        return xy_loss
+
+    def calc_wh_loss(self,true_obj,true_wh,pred_wh,weight):
+
+        wh_loss = tf.reduce_sum(tf.square(true_wh-pred_wh),axis=-1)
+        true_obj = tf.squeeze(true_obj,axis=-1)
+
+        wh_loss = true_obj * wh_loss * weight
+        wh_loss = tf.reduce_sum(wh_loss,axis=(1,2,3)) * self.lambda_coord
+        return wh_loss
+
