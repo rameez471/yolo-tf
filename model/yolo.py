4b2cd973a4a120dd20b9b338f555982260cdbc70
rameez471 <yashpathak29@gmail.com>
2020-09-18 12:33:17 +0530

    Yolo Loss

--- model/yolo.py
+++ model/yolo.py
@@ -194,7 +194,7 @@ def yolo_eval(yolo_outputs,anchors,num_classes,image_shape,
 
     return boxes_, scores_, classes_
         
-def preprocess_true_boxes(self,input_shape,anchors,num_classes):
+def preprocess_true_boxes(true_boxes,input_shape,anchors,num_classes):
     '''Preprocess true boxes to train input'''
     assert (true_boxes[...,4] < num_classes).all(), 'Class id must be less than num_classes'
     num_layers = len(anchors) // 3
@@ -231,7 +231,7 @@ def preprocess_true_boxes(self,input_shape,anchors,num_classes):
         intersect_wh = np.maximum(intersect_maxes - intersect_mins, 0.)
         intersect_area = intersect_wh[...,0] * intersect_wh[...,1]
         box_area = wh[...,0] * wh[...,1]
-        anchor_area = anchor[...,0] * anchor[...,1]
+        anchor_area = anchors[...,0] * anchors[...,1]
         iou = intersect_area / (box_area + anchor_area - intersect_area)
         
         best_anchor = np.argmax(iou, axis=-1)
@@ -248,3 +248,85 @@ def preprocess_true_boxes(self,input_shape,anchors,num_classes):
                     y_true[l][b,j,i,k, 5+c] = 1
 
     return y_true
+
+def box_iou(b1, b2):
+    '''Return iou tensor'''
+    b1 = K.expand_dims(b1, -2)
+    b1_xy = b1[...,:2]
+    b1_wh = b1[...,2:4]
+    b1_mins = b1_xy - b1_wh/2.
+    b1_maxes = b1_xy + b1_wh/2.
+
+    b2 = K.expand_dims(b2,0)
+    b2_xy = b2[...,:2]
+    b2_wh = b2[...,2:4]
+    b2_mins = b2_xy - b2_wh/2.
+    b2_maxes = b2_xy + b2_wh/2.
+
+    intersect_mins = K.maximum(b1_mins, b2_mins)
+    intersect_maxes = K.minimum(b1_maxes, b2_maxes)
+    intersect_wh = K.maximum(intersect_maxes - intersect_mins, 0)
+    intersect_area = intersect_wh[...,0] * intersect_wh[...,1]
+    b1_area = b1_wh[...,0] * b1_wh[...,1]
+    b2_area = b2_wh[...,0] * b2_wh[...,1]
+    iou = intersect_area / (b1_area + b2_area - intersect_area)
+
+    return iou
+
+def yolo_loss(args, anchors, num_classes, ignore_threshold=0.5, print_loss=True):
+    '''Return yolo loss tensor'''
+    num_layers = len(anchors) // 3
+    yolo_outputs = args[:num_layers]
+    y_true = args[num_layers:]
+    anchor_mask = [[6, 7, 8], [3, 4, 5], [0, 1, 2]] if num_layers == 3 else [[3, 4, 5], [1, 2, 3]]
+    input_shape = K.cast(K.shape(yolo_outputs[0])[1:3] * 32,K.dtype(y_true[0]))
+    grid_shape = [K.cast(K.shape(yolo_outputs[l])[1:3], K.dtype(y_true[0])) for l in range(num_layers)]
+    loss = 0
+    m = K.shape(yolo_outputs[0])[0]
+    mf = K.cast(m, K.dtype(yolo_outputs[0]))
+
+    for l in range(num_layers):
+        object_mask = y_true[l][...,4:5]
+        true_class_probs = y_true[l][...,5:]
+
+        grid, raw_pred, pred_xy, pred_wh = yolo_head(yolo_outputs[l],
+                                            anchors[anchor_mask[l]],num_classes,input_shape,calc_loss=True)
+        pred_box = K.concatenate([pred_xy, pred_wh])
+
+        raw_true_xy = y_true[l][...,:2] * grid_shapes[l][::-1] - grid
+        raw_true_wh = K.log(y_true[l][...,2:4] / anchors[anchor_mask[l]] * input_shape[::-1])
+        raw_true_wh = K.switch(object_mask, raw_true_wh, K.zeros_like(raw_true_wh))
+        box_loss_scale = 2 - y_true[l][...,2:3] * y_true[l][...,3:4]
+
+        ignore_mask = tf.TensorArray(K.dtype(y_true[0]), size=1, dynamic_size=True)
+        object_mask_bool = K.cast(object_mask,'bool')
+
+        def loop_body(b, ignore_mask):
+            true_box = tf.boolean_mask(y_true[l][b,...,0:4], object_mask_bool[b,...,0])
+            iou = box_iou(pred_box[b], true_box)
+            best_iou = K.max(iou, axis=-1)
+            ignore_mask = ingore_mask.write(b, K.cast(best_iou < ignore_threshold, K.dtype(true_box)))
+            return b+1, ingore_mask
+
+        _, ignore_mask = tf.while_loop(lambda b: *args: b<m, loop_body.[0, ignore_mask]) 
+        ignore_mask = ignore_mask.stack()
+        ignore_mask = K.expand_dims(ignore_mask, -1)
+
+        xy_loss = object_mask * box_loss_scale * K.binary_crossentropy(raw_true_xy, raw_pred[...,0:2],
+                                                                        from_logits=True)
+        wh_loss = object_mask * box_loss_scale * 0.5 * K.square(raw_true_wh - raw_pred[...,2:4])
+        confidence_loss = object_mask * K.binary_crossentropy(object_mask,raw_pred[...,4:5], from_logits=True) + \
+                            (1-object_mask) * K.binary_crossentropy(object_mask,raw_pred[...4:5],from_logits=True) * ignore_mask
+
+        class_loss = object_mask * K.binary_crossentropy(true_class_probs, raw_pred[...,5:],from_logits=True)
+
+        xy_loss = K.sum(xy_loss) / mf
+        wh_loss = K.sum(wh_loss) / mf
+        confidence_loss = K.sum(confidence_loss) / mf
+        class_loss = K.sum(class_loss) / mf
+        loss += xy_loss + wh_loss + confidence_loss + class_loss
+        if print_loss:
+            print('Loss: ',loss)
+
+    return loss
+
